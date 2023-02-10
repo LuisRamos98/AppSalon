107924e472d78a88ed07a536868890c78159215d
Luis Ramos Moncayo <luisandres11061998@gmail.com>
2023-02-10 16:59:46 +0100

    creacion del index/login

---
+++ controllers/LoginController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Controllers;
+
+use MVC\Router;
+
+class LoginController {
+    public static function login(Router $router) {
+        
+        $router->render('auth/login');
+    }
+
+    public static function logout() {
+        echo "DESDE LOGOUT";
+    }
+
+    public static function olvide() {
+        echo "DESDE OLVIDE";
+    }
+
+    public static function recuperar() {
+        echo "DESDE RECUPERAR";
+    }
+
+    public static function crear() {
+        echo "DESDE CREAR";
+    }
+}
