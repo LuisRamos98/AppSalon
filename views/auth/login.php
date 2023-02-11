220bb2c21906b7ef83f90ba550c4f8ce760dd406
Luis Ramos Moncayo <luisandres11061998@gmail.com>
2023-02-11 21:39:11 +0100

    creando la seccion de Login

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,1 +1,30 @@
-<h1>LOGIN</h1>
+<h1 class="nombre-pagina">LOGIN</h1>
+<p class="descripcion-pagina">Inicie sesión con sus datos</p>
+
+<form action="/" method="POST" class="formulario">
+    <div class="campo">
+        <label for="email">Email</label>
+        <input 
+            type="email"
+            id="email"
+            placeholder="Tu Email"
+            name="email"
+        >
+    </div>
+    <div class="campo">
+        <label for="password">Password</label>
+        <input 
+            type="password"
+            id="password"
+            placeholder="Tu Password"
+            name="password"
+        >
+    </div>
+
+    <input type="submit" class="boton" value="Iniciar Sesión">
+</form>
+
+<div class="acciones">
+    <a href="/crear-cuenta">¿Aún no tienes una cuenta? Crear una</a>
+    <a href="/olvide">¿Olvidaste tu password?</a>
+</div>
