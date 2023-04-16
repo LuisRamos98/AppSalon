9b3f1a9fcb2023869a59b708fdf1ca6251504dea
LuisRamos <luisandres11061998@gmail.com>
2023-04-16 14:55:19 +0200

    añadiendo el CRUD de servicios

--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@ use Controllers\AdminController;
 use Controllers\APIController;
 use Controllers\CitaController;
 use Controllers\LoginController;
+use Controllers\ServicioController;
 use MVC\Router;
 
 $router = new Router();
@@ -39,6 +40,14 @@ $router->get("/admin",[AdminController::class,"index"]);
 $router->get("/api/servicios", [APIController::class,'index']);
 $router->post("/api/citas", [APIController::class,'guardar']);
 $router->post("/api/eliminar", [APIController::class,'eliminar']);
+
+//  CRUD de Servicios
+$router->get("/servicios", [ServicioController::class,'index']);
+$router->get("/servicios/crear", [ServicioController::class,'crear']);
+$router->get("/servicios/crear", [ServicioController::class,'crear']);
+$router->get("/servicios/actualizar", [ServicioController::class,'actualizar']);
+$router->get("/servicios/actualizar", [ServicioController::class,'actualizar']);
+$router->post("/servicios/eliminar", [ServicioController::class,'eliminar']);
 
 
 
